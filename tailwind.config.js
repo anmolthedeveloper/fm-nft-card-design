71b3fa7b47f16bb9aa254f4a06f2a902cab8fd89
Anmol Singh Tuteja <anmolthedeveloper@gmail.com>
2023-05-07 21:06:19 +0530

    created design

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      content: {
+        nft: 'url("/images/icon-ethereum.svg")',
+        clock: 'url("/images/icon-clock.svg")',
+        avatar: 'url("/images/image-avatar.png")',
+      },
+      colors: {
+        mainBG: "hsl(217, 54%, 11%)",
+        cardBG: "hsl(216, 50%, 16%)",
+        line: "hsl(215, 32%, 27%)",
+        cyanText: "hsl(178, 100%, 50%)",
+        cyanImg: "rgba(0, 255, 247, 0.5);",
+        text: "hsl(215, 51%, 70%)",
+      },
+      fontFamily: {
+        outfit: ["Outfit"],
+      },
+    },
   },
   plugins: [],
 };
